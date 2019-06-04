666f44d9ddc20a57e84c432d50635cb37a856c6f
Mr-emeka <speak2c.emeka@gmail.com>
2019-06-04 17:09:12 +0100

    blog version2.0

---
+++ assets/js/main.js
@@ -0,0 +1,132 @@
+var posts = [{
+          id: 1,
+          title: 'How to cook rice',
+          date: '20th, April 2020',
+          image: "url('https://i.imgur.com/0yRWYR6.jpg')",
+          content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi atque ab aliquid repellendus dignissimos harum, assumenda inventore delectus consequatur voluptates eveniet, vel quo veniam modi temporibus, dolorum distinctio non eligendi cupiditate quidem. Velit qui adipisci accusamus nulla earum corrupti temporibus repellat nihil sint molestiae voluptatum facilis, ab illum praesentium fugit maxime ea aliquam esse possimus cum cumque. Aut, perspiciatis minima pariatur sed provident a earum repudiandae dignissimos. Quo laudantium, non eaque aspernatur fugit corrupti excepturi libero veniam doloribus cumque dicta, ex commodi quaerat incidunt tempora tempore quod asperiores illo placeat inventore porro. Cum ab culpa illum animi dolore explicabo quaerat aspernatur earum voluptates dolorum quibusdam laudantium iste veniam, porro repellendus tenetur consequuntur. At quidem tenetur numquam odit maiores ipsum quos quasi qui dolore quaerat consequuntur, explicabo harum omnis exercitationem cupiditate aspernatur laboriosam? Optio ducimus nemo aliquam est doloribus eligendi deserunt fugit sit accusantium ipsam laborum, odit voluptate ratione nihil modi voluptatem, perferendis recusandae deleniti quis nisi? Ex magnam temporibus earum illum, iure perspiciatis harum vero, debitis quas, maxime quis. Modi perferendis ducimus fuga, officiis hic nihil odit incidunt porro excepturi error aspernatur nulla ab sint quod iure eius praesentium quibusdam illum dicta doloribus id quas! Ut ratione commodi, fugit suscipit doloremque doloribus mollitia molestiae sequi eveniet quod, aliquam sed alias amet, deserunt ea iusto illum quidem facere dicta eos! Animi vel tempore omnis magni doloremque facilis iusto, voluptatem iure quae, molestiae nesciunt repellat eius quis exercitationem architecto distinctio pariatur deserunt placeat cumque ab ea aut velit. Sequi debitis officiis reprehenderit tenetur iure praesentium nemo facere, delectus illum voluptas, ",
+          pageurl: '../../page1.html'
+     },
+     {
+          id: 2,
+          title: 'How to cook Coconut rice',
+          date: '20th, April 2020',
+          image: "url('https://i.imgur.com/b636RJG.jpg')",
+          content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi atque ab aliquid repellendus dignissimos harum, assumenda inventore delectus consequatur voluptates eveniet, vel quo veniam modi temporibus, dolorum distinctio non eligendi cupiditate quidem. Velit qui adipisci accusamus nulla earum corrupti temporibus repellat nihil sint molestiae voluptatum facilis, ab illum praesentium fugit maxime ea aliquam esse possimus cum cumque. Aut, perspiciatis minima pariatur sed provident a earum repudiandae dignissimos. Quo laudantium, non eaque aspernatur fugit corrupti excepturi libero veniam doloribus cumque dicta, ex commodi quaerat incidunt tempora tempore quod asperiores illo placeat inventore porro. Cum ab culpa illum animi dolore explicabo quaerat aspernatur earum voluptates dolorum quibusdam laudantium iste veniam, porro repellendus tenetur consequuntur. At quidem tenetur numquam odit maiores ipsum quos quasi qui dolore quaerat consequuntur, explicabo harum omnis exercitationem cupiditate aspernatur laboriosam? Optio ducimus nemo aliquam est doloribus eligendi deserunt fugit sit accusantium ipsam laborum, odit voluptate ratione nihil modi voluptatem, perferendis recusandae deleniti quis nisi? Ex magnam temporibus earum illum, iure perspiciatis harum vero, debitis quas, maxime quis. Modi perferendis ducimus fuga, officiis hic nihil odit incidunt porro excepturi error aspernatur nulla ab sint quod iure eius praesentium quibusdam illum dicta doloribus id quas! Ut ratione commodi, fugit suscipit doloremque doloribus mollitia molestiae sequi eveniet quod, aliquam sed alias amet, deserunt ea iusto illum quidem facere dicta eos! Animi vel tempore omnis magni doloremque facilis iusto, voluptatem iure quae, molestiae nesciunt repellat eius quis exercitationem architecto distinctio pariatur deserunt placeat cumque ab ea aut velit. Sequi debitis officiis reprehenderit tenetur iure praesentium nemo facere, delectus illum voluptas,",
+          pageurl: '../../page2.html'
+     },
+     {
+          id: 3,
+          title: 'How to cook Indian spice rice',
+          date: '20th, April 2020',
+          image: "url('https://i.imgur.com/wggq1UX.jpg')",
+          content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi atque ab aliquid repellendus dignissimos harum, assumenda inventore delectus consequatur voluptates eveniet, vel quo veniam modi temporibus, dolorum distinctio non eligendi cupiditate quidem. Velit qui adipisci accusamus nulla earum corrupti temporibus repellat nihil sint molestiae voluptatum facilis, ab illum praesentium fugit maxime ea aliquam esse possimus cum cumque. Aut, perspiciatis minima pariatur sed provident a earum repudiandae dignissimos. Quo laudantium, non eaque aspernatur fugit corrupti excepturi libero veniam doloribus cumque dicta, ex commodi quaerat incidunt tempora tempore quod asperiores illo placeat inventore porro. Cum ab culpa illum animi dolore explicabo quaerat aspernatur earum voluptates dolorum quibusdam laudantium iste veniam, porro repellendus tenetur consequuntur. At quidem tenetur numquam odit maiores ipsum quos quasi qui dolore quaerat consequuntur, explicabo harum omnis exercitationem cupiditate aspernatur laboriosam? Optio ducimus nemo aliquam est doloribus eligendi deserunt fugit sit accusantium ipsam laborum, odit voluptate ratione nihil modi voluptatem, perferendis recusandae deleniti quis nisi? Ex magnam temporibus earum illum, iure perspiciatis harum vero, debitis quas, maxime quis. Modi perferendis ducimus fuga, officiis hic nihil odit incidunt porro excepturi error aspernatur nulla ab sint quod iure eius praesentium quibusdam illum dicta doloribus id quas! Ut ratione commodi, fugit suscipit doloremque doloribus mollitia molestiae sequi eveniet quod, aliquam sed alias amet, deserunt ea iusto illum quidem facere dicta eos! Animi vel tempore omnis magni doloremque facilis iusto, voluptatem iure quae, molestiae nesciunt repellat eius quis exercitationem architecto distinctio pariatur deserunt placeat cumque ab ea aut velit. Sequi debitis officiis reprehenderit tenetur iure praesentium nemo facere, delectus illum voluptas,",
+          pageurl: '../../page3.html'
+     },
+     {
+          id: 4,
+          title: 'How to cook Chicken rice',
+          date: '20th, April 2020',
+          image: "url('https://i.imgur.com/pVsNelQ.jpg')",
+          content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi atque ab aliquid repellendus dignissimos harum, assumenda inventore delectus consequatur voluptates eveniet, vel quo veniam modi temporibus, dolorum distinctio non eligendi cupiditate quidem. Velit qui adipisci accusamus nulla earum corrupti temporibus repellat nihil sint molestiae voluptatum facilis, ab illum praesentium fugit maxime ea aliquam esse possimus cum cumque. Aut, perspiciatis minima pariatur sed provident a earum repudiandae dignissimos. Quo laudantium, non eaque aspernatur fugit corrupti excepturi libero veniam doloribus cumque dicta, ex commodi quaerat incidunt tempora tempore quod asperiores illo placeat inventore porro. Cum ab culpa illum animi dolore explicabo quaerat aspernatur earum voluptates dolorum quibusdam laudantium iste veniam, porro repellendus tenetur consequuntur. At quidem tenetur numquam odit maiores ipsum quos quasi qui dolore quaerat consequuntur, explicabo harum omnis exercitationem cupiditate aspernatur laboriosam? Optio ducimus nemo aliquam est doloribus eligendi deserunt fugit sit accusantium ipsam laborum, odit voluptate ratione nihil modi voluptatem, perferendis recusandae deleniti quis nisi? Ex magnam temporibus earum illum, iure perspiciatis harum vero, debitis quas, maxime quis. Modi perferendis ducimus fuga, officiis hic nihil odit incidunt porro excepturi error aspernatur nulla ab sint quod iure eius praesentium quibusdam illum dicta doloribus id quas! Ut ratione commodi, fugit suscipit doloremque doloribus mollitia molestiae sequi eveniet quod, aliquam sed alias amet, deserunt ea iusto illum quidem facere dicta eos! Animi vel tempore omnis magni doloremque facilis iusto, voluptatem iure quae, molestiae nesciunt repellat eius quis exercitationem architecto distinctio pariatur deserunt placeat cumque ab ea aut velit. Sequi debitis officiis reprehenderit tenetur iure praesentium nemo facere, delectus illum voluptas, ",
+          pageurl: '../../page4.html'
+     },
+     {
+          id: 5,
+          title: 'How to cook Pineapple lime rice',
+          date: '20th, April 2020',
+          image: "url('https://i.imgur.com/piFHhyv.jpg')",
+          content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi atque ab aliquid repellendus dignissimos harum, assumenda inventore delectus consequatur voluptates eveniet, vel quo veniam modi temporibus, dolorum distinctio non eligendi cupiditate quidem. Velit qui adipisci accusamus nulla earum corrupti temporibus repellat nihil sint molestiae voluptatum facilis, ab illum praesentium fugit maxime ea aliquam esse possimus cum cumque. Aut, perspiciatis minima pariatur sed provident a earum repudiandae dignissimos. Quo laudantium, non eaque aspernatur fugit corrupti excepturi libero veniam doloribus cumque dicta, ex commodi quaerat incidunt tempora tempore quod asperiores illo placeat inventore porro. Cum ab culpa illum animi dolore explicabo quaerat aspernatur earum voluptates dolorum quibusdam laudantium iste veniam, porro repellendus tenetur consequuntur. At quidem tenetur numquam odit maiores ipsum quos quasi qui dolore quaerat consequuntur, explicabo harum omnis exercitationem cupiditate aspernatur laboriosam? Optio ducimus nemo aliquam est doloribus eligendi deserunt fugit sit accusantium ipsam laborum, odit voluptate ratione nihil modi voluptatem, perferendis recusandae deleniti quis nisi? Ex magnam temporibus earum illum, iure perspiciatis harum vero, debitis quas, maxime quis. Modi perferendis ducimus fuga, officiis hic nihil odit incidunt porro excepturi error aspernatur nulla ab sint quod iure eius praesentium quibusdam illum dicta doloribus id quas! Ut ratione commodi, fugit suscipit doloremque doloribus mollitia molestiae sequi eveniet quod, aliquam sed alias amet, deserunt ea iusto illum quidem facere dicta eos! Animi vel tempore omnis magni doloremque facilis iusto, voluptatem iure quae, molestiae nesciunt repellat eius quis exercitationem architecto distinctio pariatur deserunt placeat cumque ab ea aut velit. Sequi debitis officiis reprehenderit tenetur iure praesentium nemo facere, delectus illum voluptas, ",
+          pageurl: '../../page5.html'
+     },
+     {
+          id: 6,
+          title: 'How to cook Soy ginger rice',
+          date: '20th, April 2020',
+          image: "url('https://i.imgur.com/C7RaNTA.jpg')",
+          content: " Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi atque ab aliquid repellendus dignissimos harum, assumenda inventore delectus consequatur voluptates eveniet, vel quo veniam modi temporibus, dolorum distinctio non eligendi cupiditate quidem. Velit qui adipisci accusamus nulla earum corrupti temporibus repellat nihil sint molestiae voluptatum facilis, ab illum praesentium fugit maxime ea aliquam esse possimus cum cumque. Aut, perspiciatis minima pariatur sed provident a earum repudiandae dignissimos. Quo laudantium, non eaque aspernatur fugit corrupti excepturi libero veniam doloribus cumque dicta, ex commodi quaerat incidunt tempora tempore quod asperiores illo placeat inventore porro. Cum ab culpa illum animi dolore explicabo quaerat aspernatur earum voluptates dolorum quibusdam laudantium iste veniam, porro repellendus tenetur consequuntur. At quidem tenetur numquam odit maiores ipsum quos quasi qui dolore quaerat consequuntur, explicabo harum omnis exercitationem cupiditate aspernatur laboriosam? Optio ducimus nemo aliquam est doloribus eligendi deserunt fugit sit accusantium ipsam laborum, odit voluptate ratione nihil modi voluptatem, perferendis recusandae deleniti quis nisi? Ex magnam temporibus earum illum, iure perspiciatis harum vero, debitis quas, maxime quis. Modi perferendis ducimus fuga, officiis hic nihil odit incidunt porro excepturi error aspernatur nulla ab sint quod iure eius praesentium quibusdam illum dicta doloribus id quas! Ut ratione commodi, fugit suscipit doloremque doloribus mollitia molestiae sequi eveniet quod, aliquam sed alias amet, deserunt ea iusto illum quidem facere dicta eos! Animi vel tempore omnis magni doloremque facilis iusto, voluptatem iure quae, molestiae nesciunt repellat eius quis exercitationem architecto distinctio pariatur deserunt placeat cumque ab ea aut velit. Sequi debitis officiis reprehenderit tenetur iure praesentium nemo facere, delectus illum voluptas, ",
+          pageurl: '../../page6.html'
+     },
+     {
+          id: 7,
+          title: 'How to cook Spicy green rice',
+          date: '20th, April 2020',
+          image: "url('https://i.imgur.com/qcumOiX.jpg')",
+          content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi atque ab aliquid repellendus dignissimos harum, assumenda inventore delectus consequatur voluptates eveniet, vel quo veniam modi temporibus, dolorum distinctio non eligendi cupiditate quidem. Velit qui adipisci accusamus nulla earum corrupti temporibus repellat nihil sint molestiae voluptatum facilis, ab illum praesentium fugit maxime ea aliquam esse possimus cum cumque. Aut, perspiciatis minima pariatur sed provident a earum repudiandae dignissimos. Quo laudantium, non eaque aspernatur fugit corrupti excepturi libero veniam doloribus cumque dicta, ex commodi quaerat incidunt tempora tempore quod asperiores illo placeat inventore porro. Cum ab culpa illum animi dolore explicabo quaerat aspernatur earum voluptates dolorum quibusdam laudantium iste veniam, porro repellendus tenetur consequuntur. At quidem tenetur numquam odit maiores ipsum quos quasi qui dolore quaerat consequuntur, explicabo harum omnis exercitationem cupiditate aspernatur laboriosam? Optio ducimus nemo aliquam est doloribus eligendi deserunt fugit sit accusantium ipsam laborum, odit voluptate ratione nihil modi voluptatem, perferendis recusandae deleniti quis nisi? Ex magnam temporibus earum illum, iure perspiciatis harum vero, debitis quas, maxime quis. Modi perferendis ducimus fuga, officiis hic nihil odit incidunt porro excepturi error aspernatur nulla ab sint quod iure eius praesentium quibusdam illum dicta doloribus id quas! Ut ratione commodi, fugit suscipit doloremque doloribus mollitia molestiae sequi eveniet quod, aliquam sed alias amet, deserunt ea iusto illum quidem facere dicta eos! Animi vel tempore omnis magni doloremque facilis iusto, voluptatem iure quae, molestiae nesciunt repellat eius quis exercitationem architecto distinctio pariatur deserunt placeat cumque ab ea aut velit. Sequi debitis officiis reprehenderit tenetur iure praesentium nemo facere, delectus illum voluptas,",
+          pageurl: '../../page7.html'
+     },
+     {
+          id: 8,
+          title: 'How to cook Lemon dill rice',
+          date: '20th, April 2020',
+          image: "url('https://i.imgur.com/Y14Rtk2.jpg')",
+          content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi atque ab aliquid repellendus dignissimos harum, assumenda inventore delectus consequatur voluptates eveniet, vel quo veniam modi temporibus, dolorum distinctio non eligendi cupiditate quidem. Velit qui adipisci accusamus nulla earum corrupti temporibus repellat nihil sint molestiae voluptatum facilis, ab illum praesentium fugit maxime ea aliquam esse possimus cum cumque. Aut, perspiciatis minima pariatur sed provident a earum repudiandae dignissimos. Quo laudantium, non eaque aspernatur fugit corrupti excepturi libero veniam doloribus cumque dicta, ex commodi quaerat incidunt tempora tempore quod asperiores illo placeat inventore porro. Cum ab culpa illum animi dolore explicabo quaerat aspernatur earum voluptates dolorum quibusdam laudantium iste veniam, porro repellendus tenetur consequuntur. At quidem tenetur numquam odit maiores ipsum quos quasi qui dolore quaerat consequuntur, explicabo harum omnis exercitationem cupiditate aspernatur laboriosam? Optio ducimus nemo aliquam est doloribus eligendi deserunt fugit sit accusantium ipsam laborum, odit voluptate ratione nihil modi voluptatem, perferendis recusandae deleniti quis nisi? Ex magnam temporibus earum illum, iure perspiciatis harum vero, debitis quas, maxime quis. Modi perferendis ducimus fuga, officiis hic nihil odit incidunt porro excepturi error aspernatur nulla ab sint quod iure eius praesentium quibusdam illum dicta doloribus id quas! Ut ratione commodi, fugit suscipit doloremque doloribus mollitia molestiae sequi eveniet quod, aliquam sed alias amet, deserunt ea iusto illum quidem facere dicta eos! Animi vel tempore omnis magni doloremque facilis iusto, voluptatem iure quae, molestiae nesciunt repellat eius quis exercitationem architecto distinctio pariatur deserunt placeat cumque ab ea aut velit. Sequi debitis officiis reprehenderit tenetur iure praesentium nemo facere, delectus illum voluptas, ",
+          pageurl: '../../page8.html'
+     },
+]
+
+text_truncate = function (str, length, ending) {
+     if (length == null) {
+          length = 100;
+     }
+     if (ending == null) {
+          ending = '...';
+     }
+     if (str.length > length) {
+          return str.substring(0, length - ending.length) + ending;
+     } else {
+          return str;
+     }
+};
+const deleteItem = (event) => {
+     console.log(event)
+}
+
+var trend = document.querySelector('.trend');
+
+posts.forEach(post => {
+     var divcol4 = document.createElement('div');
+     var div_trendImage = document.createElement('div');
+     var div_trendContent = document.createElement('div')
+     var header = document.createElement('h3');
+     var spn = document.createElement('span');
+     var btn = document.createElement('button');
+
+     var p = document.createElement('p')
+     var link = document.createElement('a');
+
+     link.href = post.pageurl;
+
+     div_trendImage.classList.add('trend-image');
+     btn.classList.add('delete');
+     btn.textContent = 'delete';
+
+     div_trendImage.style.backgroundImage = post.image;
+
+     div_trendContent.classList.add('trend-content');
+     link.classList.add('trend-link');
+
+     divcol4.appendChild(div_trendImage);
+     divcol4.appendChild(div_trendContent);
+
+
+     header.append(link);
+     trend.append(divcol4);
+     divcol4.classList.add('col4')
+
+     div_trendContent.append(header);
+     div_trendContent.append(spn);
+     div_trendContent.append(p);
+     div_trendContent.append(btn);
+
+     spn.textContent = post.date;
+     p.textContent = text_truncate(post.content, 100, '.');
+     link.textContent = post.title;
+
+     btn.addEventListener('click', () => {
+          // console.log(divcol4)
+          divcol4.style.display = "none";
+
+          alert(`${post.title} deleted Successfully`)
+     })
+
+});
